39bee5fd100044528c9c83d0865cccdd690bbc18
Arina Solovjova <ari.solovjova@gmail.com>
2020-12-20 19:04:37 +0200

    Remove not needed components

--- Startup.cs
+++ Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using VPL_course.Data;
 
 namespace VPL_course
 {
@@ -28,7 +27,6 @@ namespace VPL_course
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
