fe1e02cfcac00e7e2d4f9853bb2d09e08b8579e9
Kristofers Volkovs <kristofers.volkovs@hwswservices.com>
2020-12-25 14:49:01 +0200

    ML: Data: created variables for storing path and mnist data
    All data is static and stored in the memory

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
-﻿using System;
+using System;
+using System.IO;
 
 namespace VPL_course
 {
     class Program
     {
+        private static string pixelTrainingFile = @"./training-data/train-images-idx3-ubyte";
+        private static string labelTrainingFile = @"./training-data/train-labels-idx1-ubyte";
+        private static MnistImg[] MnistImages = null;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
