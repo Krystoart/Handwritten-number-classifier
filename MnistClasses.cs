c60924cdbced65ebbf3f58d968aefe26106a0784
Kristofers Volkovs <kristofers.volkovs@hwswservices.com>
2020-12-27 08:50:09 +0200

    ML: Data: moved LoadData method to  `MnistList` class
    Also added some basic dataset checks to
    make sure that the data that is being
    read is correct

--- MnistClasses.cs
+++ MnistClasses.cs
@@ -9,8 +9,73 @@ class MnistList : IEnumerable<MnistItem>
     public int Length { get; }
     public int Rows { get; }
     public int Columns { get; }
-}
-}
+
+    public MnistList(string imagePath, string labelPath, bool normalize = false)
+    {
+        FileStream fsImages = new FileStream(imagePath, FileMode.Open); // Images
+        FileStream fsLabels = new FileStream(labelPath, FileMode.Open); // Labels
+
+        BinaryReader brImages = new BinaryReader(fsImages);
+        BinaryReader brLabels = new BinaryReader(fsLabels);
+
+        int magic1 = ReverseBytes(brImages.ReadInt32());
+        int magic2 = ReverseBytes(brLabels.ReadInt32());
+
+        if (magic1 != 2051)
+            throw new Exception("Not a valid MNIST image data set");
+
+        if (magic2 != 2049)
+            throw new Exception("Not a valid MNIST label data set");
+
+        int imgCount = ReverseBytes(brImages.ReadInt32());
+        int labelCount = ReverseBytes(brLabels.ReadInt32());
+
+        if (imgCount != labelCount)
+            throw new Exception("Number of items of the two files is not the same");
+
+        int imgRows = ReverseBytes(brImages.ReadInt32());
+        int imgCols = ReverseBytes(brImages.ReadInt32());
+
+        this.Length = imgCount;
+        if (normalize) // TODO see if I even need this
+        {
+            this.Rows = 1;
+            this.Columns = imgRows * imgCols;
+        }
+        else
+        {
+            this.Rows = imgRows;
+            this.Columns = imgCols;
+        }
+
+        this.images = new MnistItem[this.Length];
+        byte[][] item = new byte[Rows][];
+        for (int i = 0; i < item.Length; i++)
+            item[i] = new byte[Columns];
+
+        for (int di = 0; di < this.Length; ++di)
+        {
+            for (int i = 0; i < item.Length; ++i)
+            {
+                for (int j = 0; j < item.Length; j++)
+                {
+                    byte b = brImages.ReadByte();
+                    item[i][j] = b;
+                }
+            }
+            byte label = brLabels.ReadByte();
+
+            MnistItem newImg = new MnistItem(width: 28, height: 28, pixels: item, label: label);
+            images[di] = newImg;
+            // Console.WriteLine(newImg.ToString());
+            // Console.ReadLine();
+        }
+
+        fsImages.Close();
+        brImages.Close();
+        fsLabels.Close();
+        brLabels.Close();
+    }
 
 public class MnistItem
 {
--- Program.cs
+++ Program.cs
@@ -9,72 +9,16 @@ namespace VPL_course
         private static string labelTrainingFile = @"./training-data/train-labels-idx1-ubyte";
         private static string pixelTestingFile = @"./training-data/t10k-images-idx3-ubyte";
         private static string labelTestingFile = @"./training-data/t10k-labels-idx1-ubyte";
-        private static MnistImg[] MnistImages = null;
 
         static void Main(string[] args)
         {
-            MnistImages = LoadData(pixelTrainingFile, labelTrainingFile, 2);
-        }
-
-        private static MnistImg[] LoadData(string pixelFilePath, string labelFilePath, int imgAmount)
-        {
             try
             {
-                Console.WriteLine("Reading data in memory\n");
-
-                FileStream fsImages = new FileStream(pixelFilePath, FileMode.Open); // Images
-                FileStream fsLabels = new FileStream(labelFilePath, FileMode.Open); // Labels
-
-                BinaryReader brImages = new BinaryReader(fsImages);
-                BinaryReader brLabels = new BinaryReader(fsLabels);
-
-                MnistImg[] results = new MnistImg[imgAmount];
-                byte[][] pixels = new byte[28][];
-                for (int i = 0; i < pixels.Length; i++)
-                    pixels[i] = new byte[28];
-
-                // Discard first values
-                int magic1 = brImages.ReadInt32();
-                int imgCount = brImages.ReadInt32();
-                int numRows = brImages.ReadInt32();
-                int numCols = brImages.ReadInt32();
-
-                int magic2 = brLabels.ReadInt32();
-                int numLabels = brLabels.ReadInt32();
-
-                for (int di = 0; di < imgAmount; ++di)
-                {
-                    for (int i = 0; i < pixels.Length; ++i)
-                    {
-                        for (int j = 0; j < pixels.Length; j++)
-                        {
-                            byte b = brImages.ReadByte();
-                            pixels[i][j] = b;
-                        }
-                    }
-                    byte label = brLabels.ReadByte();
-
-                    MnistImg newImg = new MnistImg(width: 28, height: 28, pixels: pixels, label: label);
-                    results[di] = newImg;
-                    // Console.WriteLine(newImg.ToString());
-                    // Console.ReadLine();
-                }
-
-                fsImages.Close();
-                brImages.Close();
-                fsLabels.Close();
-                brLabels.Close();
-
-                Console.WriteLine("Data read in memory\n");
-                Console.ReadLine();
-
-                return results;
+                MnistList trainingData = new MnistList(imagePath: pixelTrainingFile, labelPath: labelTrainingFile);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
-                Console.ReadLine();
-                return null;
             }
         }
     }
