82c89ed6d9babebb20fb129199a7aa26ba2ddfa2
Kristofers Volkovs <kristofers.volkovs@hwswservices.com>
2020-12-30 21:59:04 +0200

    ML.NET: Model: created variabled that load the model in memory

--- Model/MnistClassificator.cs
+++ Model/MnistClassificator.cs
@@ -11,6 +11,9 @@ namespace MnistClassificator
         private static string ModelRelativePath = $"{BaseModelsRelativePath}/Model.zip";
         private static string ModelPath = GetAbsolutePath(ModelRelativePath);
 
+        private static MLContext mlContext = new MLContext();
+        private static ITransformer trainedModel = mlContext.Model.Load(ModelPath, out var modelInputSchema);
+
 
     }
 
