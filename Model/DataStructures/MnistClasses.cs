72a5b43150ea17363dbffac20305acdd0401d33e
Kristofers Volkovs <kristofers.volkovs@hwswservices.com>
2020-12-31 16:34:47 +0200

    ML.NET: Model: created new Mnist input and output classes

---
+++ Model/DataStructures/MnistClasses.cs
@@ -0,0 +1,33 @@
+using Microsoft.ML.Data;
+
+public class MnistItem
+{
+    [ColumnName("Pixels")]
+    [VectorType(784)]
+    public float[] Pixels { get; } // 0 - 1 (white - black)
+    public int Width { get; } // 28
+    public int Height { get; } // 28
+    public MnistItem(int width, int height, byte[][] pixels)
+    {
+        this.Width = width;
+        this.Height = height;
+
+        this.Pixels = new float[height*width];
+
+        int counter = 0;
+        for (int i = 0; i < height; ++i)
+        {
+            for (int j = 0; j < width; ++j)
+            {
+                this.Pixels[counter] = ((float)pixels[i][j]) / byte.MaxValue;
+                counter += 1;
+            }
+        }
+    }
+}
+
+class MnistOutPutData
+{
+    [ColumnName("Score")]
+    public float[] Score;
+}
