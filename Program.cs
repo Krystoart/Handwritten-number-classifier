c1e0cc54a6b16bd1b1f60e6bc2b37d4ea78c69ac
Kristofers Volkovs <kristofers.volkovs@hwswservices.com>
2020-12-31 15:28:47 +0200

    ML: Model: created function for demonstrating the models accuracy

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 using System;
+using Microsoft.ML;
+using System.IO;
+using VPL_course.DataStructures;
+using Microsoft.ML.Transforms;
+
 
 namespace VPL_course
 {
@@ -64,6 +69,66 @@ namespace VPL_course
             {
                 Console.WriteLine(ex.ToString());
             }
+        }
+
+        public static string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+            return fullPath;
+        }
+
+        private static void TestSomePredictions(MLContext mlContext)
+        {
+            ITransformer trainedModel = mlContext.Model.Load(ModelPath, out var modelInputSchema);
+
+            // Create prediction engine related to the loaded trained model
+            var predEngine = mlContext.Model.CreatePredictionEngine<MnistItem, MnistOutPutData>(trainedModel);
+
+            var resultprediction1 = predEngine.Predict(SampleMNISTData.MNIST1);
+
+            Console.WriteLine($"Actual: 1     Predicted probability:       zero:  {resultprediction1.Score[0]:0.####}");
+            Console.WriteLine($"                                           One :  {resultprediction1.Score[1]:0.####}");
+            Console.WriteLine($"                                           two:   {resultprediction1.Score[2]:0.####}");
+            Console.WriteLine($"                                           three: {resultprediction1.Score[3]:0.####}");
+            Console.WriteLine($"                                           four:  {resultprediction1.Score[4]:0.####}");
+            Console.WriteLine($"                                           five:  {resultprediction1.Score[5]:0.####}");
+            Console.WriteLine($"                                           six:   {resultprediction1.Score[6]:0.####}");
+            Console.WriteLine($"                                           seven: {resultprediction1.Score[7]:0.####}");
+            Console.WriteLine($"                                           eight: {resultprediction1.Score[8]:0.####}");
+            Console.WriteLine($"                                           nine:  {resultprediction1.Score[9]:0.####}");
+            Console.WriteLine();
+
+            var resultprediction2 = predEngine.Predict(SampleMNISTData.MNIST2);
+
+            Console.WriteLine($"Actual: 7     Predicted probability:       zero:  {resultprediction2.Score[0]:0.####}");
+            Console.WriteLine($"                                           One :  {resultprediction2.Score[1]:0.####}");
+            Console.WriteLine($"                                           two:   {resultprediction2.Score[2]:0.####}");
+            Console.WriteLine($"                                           three: {resultprediction2.Score[3]:0.####}");
+            Console.WriteLine($"                                           four:  {resultprediction2.Score[4]:0.####}");
+            Console.WriteLine($"                                           five:  {resultprediction2.Score[5]:0.####}");
+            Console.WriteLine($"                                           six:   {resultprediction2.Score[6]:0.####}");
+            Console.WriteLine($"                                           seven: {resultprediction2.Score[7]:0.####}");
+            Console.WriteLine($"                                           eight: {resultprediction2.Score[8]:0.####}");
+            Console.WriteLine($"                                           nine:  {resultprediction2.Score[9]:0.####}");
+            Console.WriteLine();
+
+            var resultprediction3 = predEngine.Predict(SampleMNISTData.MNIST3);
+
+            Console.WriteLine($"Actual: 9     Predicted probability:       zero:  {resultprediction3.Score[0]:0.####}");
+            Console.WriteLine($"                                           One :  {resultprediction3.Score[1]:0.####}");
+            Console.WriteLine($"                                           two:   {resultprediction3.Score[2]:0.####}");
+            Console.WriteLine($"                                           three: {resultprediction3.Score[3]:0.####}");
+            Console.WriteLine($"                                           four:  {resultprediction3.Score[4]:0.####}");
+            Console.WriteLine($"                                           five:  {resultprediction3.Score[5]:0.####}");
+            Console.WriteLine($"                                           six:   {resultprediction3.Score[6]:0.####}");
+            Console.WriteLine($"                                           seven: {resultprediction3.Score[7]:0.####}");
+            Console.WriteLine($"                                           eight: {resultprediction3.Score[8]:0.####}");
+            Console.WriteLine($"                                           nine:  {resultprediction3.Score[9]:0.####}");
+            Console.WriteLine();
         }
     }
 }
